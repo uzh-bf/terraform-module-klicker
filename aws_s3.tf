c2882cf85fd26062f9151f42b235c7784fe29523
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-10-03 12:27:05 +0200

    Add cors rules to klicker module

--- aws_s3.tf
+++ aws_s3.tf
@@ -1,14 +1,95 @@
 resource "aws_s3_bucket" "klicker_dev" {
   bucket = "${var.aws_s3_bucket_base}-dev"
   acl    = "private"
+
+  cors_rule {
+    allowed_headers = [
+      "*",
+    ]
+    allowed_methods = [
+      "GET",
+    ]
+    allowed_origins = [
+      "*",
+    ]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+  cors_rule {
+    allowed_headers = [
+      "*",
+    ]
+    allowed_methods = [
+      "PUT",
+    ]
+    allowed_origins = [
+      "http://localhost:3000",
+    ]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
 }
 
 resource "aws_s3_bucket" "klicker_stage" {
   bucket = "${var.aws_s3_bucket_base}-stage"
   acl    = "private"
+
+  cors_rule {
+    allowed_headers = [
+      "*",
+    ]
+    allowed_methods = [
+      "GET",
+    ]
+    allowed_origins = [
+      "*",
+    ]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+  cors_rule {
+    allowed_headers = [
+      "*",
+    ]
+    allowed_methods = [
+      "PUT",
+    ]
+    allowed_origins = [
+      "https://klicker-react-stage.zh.shift.switchengines.ch",
+    ]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
 }
 
 resource "aws_s3_bucket" "klicker_prod" {
   bucket = "${var.aws_s3_bucket_base}-prod"
   acl    = "private"
+
+  cors_rule {
+    allowed_headers = [
+      "*",
+    ]
+    allowed_methods = [
+      "GET",
+    ]
+    allowed_origins = [
+      "https://app.klicker.uzh.ch",
+    ]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+  cors_rule {
+    allowed_headers = [
+      "*",
+    ]
+    allowed_methods = [
+      "PUT",
+    ]
+    allowed_origins = [
+      "https://app.klicker.uzh.ch",
+    ]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
 }
